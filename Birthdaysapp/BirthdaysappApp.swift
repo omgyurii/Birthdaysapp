5521a1308c9ce6cc1e32eca9164602a2108d62e7
Yuri Hiraiwa <yurihiraiwa@gmail.com>
2025-07-11 10:11:26 -0400

    Initial Commit

---
+++ Birthdaysapp/BirthdaysappApp.swift
@@ -0,0 +1,17 @@
+//
+//  BirthdaysappApp.swift
+//  Birthdaysapp
+//
+//  Created by Yuri Hiraiwa on 7/11/25.
+//
+
+import SwiftUI
+
+@main
+struct BirthdaysappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
