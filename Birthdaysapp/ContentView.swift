9dcdd9612d80f6a048cd0034cbaf6401562659b8
Yuri Hiraiwa <yurihiraiwa@gmail.com>
2025-07-11 11:34:23 -0400

    set up ui.

--- Birthdaysapp/ContentView.swift
+++ Birthdaysapp/ContentView.swift
@@ -1,21 +1,44 @@
-//
-//  ContentView.swift
-//  Birthdaysapp
-//
-//  Created by Yuri Hiraiwa on 7/11/25.
-//
-
 import SwiftUI
 
 struct ContentView: View {
+    @State private var newName = ""
+    @State private var newBirthday = Date.now
+    
+    @State private var friends: [Friend] = [
+        Friend(name: "Yuri Hiraiwa", birthday: .now),
+        Friend(name: "Pete", birthday: .now)
+    ]
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+            List(friends, id: \.name) { friend in
+                HStack {
+                    Text(friend.name)
+                    Spacer()
+                    Text(friend.birthday, format: .dateTime.month(.wide).day().year())
+                }
+            }
+            .navigationTitle("Birthdays")
+            .safeAreaInset(edge: .bottom) {
+                VStack(alignment: .center, spacing: 20) {
+                    Text("New Birthday")
+                        .font(.headline)
+                    DatePicker(selection: $newBirthday, in: Date.distantPast...Date.now, displayedComponents: .date){
+                        TextField("Name", text: $newName)
+                            .textFieldStyle(.roundedBorder)
+                    }
+                }
+                .padding()
+                .background(Color(.systemGray6))
+            }
+            Button("Save"){
+                let newFriend = Friend(name: newName, birthday: newBirthday)
+                friends.append(newFriend)
+                newName = ""
+                newBirthday = .now
+            }
+            .bold()
         }
-        .padding()
     }
 }
 
