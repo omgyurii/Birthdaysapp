d5123fcd03090f87bbef4d7152ad28bea1d80250
Yuri Hiraiwa <yurihiraiwa@gmail.com>
2025-07-11 12:49:33 -0400

    newest, added model containers.

--- Birthdaysapp/ContentView.swift
+++ Birthdaysapp/ContentView.swift
@@ -1,17 +1,17 @@
 import SwiftUI
+import SwiftData
 
 struct ContentView: View {
     @State private var newName = ""
     @State private var newBirthday = Date.now
+    //freinds array
+    @Query private var friends: [Friend]
+    @Environment(\.modelContext) private var context
     
-    @State private var friends: [Friend] = [
-        Friend(name: "Yuri Hiraiwa", birthday: .now),
-        Friend(name: "Pete", birthday: .now)
-    ]
     
     var body: some View {
         NavigationStack {
-            List(friends, id: \.name) { friend in
+            List(friends) { friend in
                 HStack {
                     Text(friend.name)
                     Spacer()
@@ -33,7 +33,7 @@ struct ContentView: View {
             }
             Button("Save"){
                 let newFriend = Friend(name: newName, birthday: newBirthday)
-                friends.append(newFriend)
+                context.insert(newFriend)
                 newName = ""
                 newBirthday = .now
             }
@@ -44,4 +44,5 @@ struct ContentView: View {
 
 #Preview {
     ContentView()
+        .modelContainer(for: Friend.self)
 }
